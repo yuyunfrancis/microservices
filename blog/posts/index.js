b671bb9907fa747c8e3745f1d59b0d2e106da2b8
yuyunfrancis <yuyunfrancis95@gmail.com>
2024-12-03 22:03:54 -0800

    event syncing

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -37,7 +37,7 @@ app.post("/posts", async (req, res) => {
 app.post("/events", (req, res) => {
   console.log("Received Event", req.body.type);
 
-  res.send({}); // Respond to the event
+  res.send({});
 });
 
 app.listen(4000, () => {
